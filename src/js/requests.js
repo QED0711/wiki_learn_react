2f19f9670c37fd05276f108fbba4f7187bf88e0d
Quinn Dizon <qdizon@gmail.com>
2019-09-04 14:36:31 -0400

    decision threshold optimizations

--- src/js/requests.js
+++ src/js/requests.js
@@ -18,12 +18,19 @@ const postArticle = (text, setRecommendations, setLoading, setDecisionThreshold)
 
     $.ajax(settings).done(response => {
         console.log(response)
-
+        let [before, after] = [0, 0]
         // breakout the 'before' and 'after' prediction probbilities before setting in state
         for (let pred of response.predictions) {
+            if(pred.position === 'before'){
+                before += 1
+            } else {
+                after += 1
+            }
             pred[response.classes[0]] = pred.label_proba[0]
             pred[response.classes[1]] = pred.label_proba[1]
         }
+
+        console.log({before, after})
 
         setLoading(false)
         setRecommendations(response)
