44e658701422ac880dbde45bd2ae8e194b7202af
Quinn Dizon <qdizon@gmail.com>
2019-09-05 06:11:55 -0400

    added loging for progress

--- src/js/requests.js
+++ src/js/requests.js
@@ -5,23 +5,46 @@ const postArticle = (text, setRecommendations, setLoading, setDecisionThreshold)
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "http://127.0.0.1:5000",
+        // "url": "http://127.0.0.1:5000",
+        "url": "https://glacial-scrubland-06464.herokuapp.com/",
         "method": "POST",
         "headers": {
             "Content-Type": "application/json",
             "cache-control": "no-cache",
+            "accept": "text/event-stream",
         },
         "timeout": 0,
         "processData": false,
-        "data": JSON.stringify({ entry: text })
+        "data": JSON.stringify({ entry: text }),
+
+        "xhr": (e) => {
+            const xhr = $.ajaxSettings.xhr();
+            xhr.onprogress = e => {
+                // For downloads
+                console.log("progress")
+                // $.ajax({
+                //     "async": true,
+                //     "crossDomain": true,
+                //     "url": "https://glacial-scrubland-06464.herokuapp.com/",
+                //     "method": "GET"
+                // }).done( response => {
+                //     console.log(response)
+                // })
+
+            };
+            return xhr;
+        }
     }
 
     $.ajax(settings).done(response => {
         console.log(response)
+        response = JSON.parse(response.split("formatted\n")[1].replace(/\'/g, '"'))
+
+        debugger
         let [before, after] = [0, 0]
         // breakout the 'before' and 'after' prediction probbilities before setting in state
         for (let pred of response.predictions) {
-            if(pred.position === 'before'){
+            if (pred.position === 'before') {
                 before += 1
             } else {
                 after += 1
@@ -30,7 +53,7 @@ const postArticle = (text, setRecommendations, setLoading, setDecisionThreshold)
             pred[response.classes[1]] = pred.label_proba[1]
         }
 
-        console.log({before, after})
+        console.log({ before, after })
 
         setLoading(false)
         setRecommendations(response)
